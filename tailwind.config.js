7940cbdb416ca73f9199b851c0200fb2ae8a4a99
buddhiko <buddhiko@outlook.com>
2022-12-02 10:13:42 +0800

    navbar: flex

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  darkMode: 'class',
-  content: ['./src/**/*.{html,ts,svg}'],
-  theme: {
-    extend: {},
-  },
+  darkMode: "class",
+  content: ["./src/**/*.{html,ts,svg}"],
+  theme: {},
   plugins: [],
 };
