41d704f9e41a8af6ce86aa89774cf23150266b45
buddhiko <buddhiko@outlook.com>
2023-02-23 09:43:23 +0800

    scrollbar: moving to public service

--- src/app/core/public.service.ts
+++ src/app/core/public.service.ts
@@ -6,6 +6,8 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 })
 export class PublicService {
   private _isLgDevice: boolean;
+  private _isScrollbarShow = false;
+  // private _scrollbarTimoutId = 0;
 
   constructor(private deviceService: DeviceDetectorService) {
     this._isLgDevice = this.deviceService.isDesktop();
@@ -17,5 +19,28 @@ export class PublicService {
 
   get atPageTop(): boolean {
     return window.scrollY <= 0;
+  }
+
+  get isScrollbarShow(): boolean {
+    return this._isScrollbarShow;
+  }
+
+  private _hideScrollbar(): void {
+    document.documentElement.classList.remove('g-scrollbar-show');
+    document.documentElement.classList.add('g-scrollbar-hide');
+    this._isScrollbarShow = false;
+  }
+
+  // fix bug of tailwindcss-scrollbar
+  showScrollbar(): void {
+    if (this._isScrollbarShow) {
+      return;
+    }
+    document.documentElement.classList.remove('g-scrollbar-hide');
+    document.documentElement.classList.add('g-scrollbar-show');
+    window.setTimeout(() => {
+      this._hideScrollbar();
+    }, 3000);
+    this._isScrollbarShow = true;
   }
 }
