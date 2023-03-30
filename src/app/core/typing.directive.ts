4e658aab2b840d6c2baeaf55f4f2e1d6e3926f41
buddhiko <buddhiko@outlook.com>
2023-03-30 19:44:09 +0800

    typing: add blinking effect.

--- src/app/core/typing.directive.ts
+++ src/app/core/typing.directive.ts
@@ -19,6 +19,32 @@ export class TypingDirective implements OnInit {
     setTimeout(this._typing.bind(this), this.delay);
   }
 
+  private _blinking(): void {
+    const speed = 700; // ms
+    const counter = 14;
+    interval(speed)
+      .pipe(
+        map((i) => i + 1),
+        take(counter)
+      )
+      .subscribe((i) => {
+        const text = this._el.nativeElement.innerHTML;
+        if (i == 1) {
+          this._el.nativeElement.innerHTML += ' ▌';
+        } else if (i == counter) {
+          this._el.nativeElement.innerHTML = text.slice(0, -2);
+        } else {
+          const lastChar = text.charAt(text.length - 1);
+          this._el.nativeElement.innerHTML = text.slice(0, -1);
+          if (lastChar == '▌') {
+            this._el.nativeElement.innerHTML += ' ';
+          } else {
+            this._el.nativeElement.innerHTML += '▌';
+          }
+        }
+      });
+  }
+
   private _typing(): void {
     interval(this.speed)
       .pipe(
@@ -26,7 +52,10 @@ export class TypingDirective implements OnInit {
         take(this._charList.length)
       )
       .subscribe((char) => {
-        this._el.nativeElement.innerHTML += char;
+        this._el.nativeElement.innerHTML += `${char}`;
+        if (this._el.nativeElement.innerHTML.length == this._charList.length) {
+          this._blinking();
+        }
       });
   }
 }
