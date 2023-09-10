f98a59220e1641127b12c33f12c3d73e12d1e438
name <name@outlook.com>
2023-09-10 16:09:55 +0800

    switch slider from transition fo animations.

--- src/app/core/slider.directive.ts
+++ src/app/core/slider.directive.ts
@@ -1,46 +1,20 @@
-import { Directive, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
-import { DeviceDetectorService } from 'ngx-device-detector';
-
-const SLIDER_CLASS = 'g-slider';
-const SLIDER_LG_CLASS = 'g-slider-lg';
-const SLIDER_ACTIVE_CLASS = 'g-slider-active';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appSlider]',
   standalone: true,
 })
-export class SliderDirective implements OnDestroy, OnInit {
+export class SliderDirective implements OnInit {
   @Input() slideDelay = 0;
+  @Input() slideDuration = 1500;
 
-  private _timeoutId = 0;
-  private _sliderClass = '';
-
-  constructor(
-    private _el: ElementRef,
-    private _deviceService: DeviceDetectorService
-  ) {}
+  constructor(private _el: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    this._sliderClass = this._deviceService.isDesktop()
-      ? SLIDER_LG_CLASS
-      : SLIDER_CLASS;
-    this._el.nativeElement.classList.add(this._sliderClass);
-    this._slideOnLoad();
-  }
-
-  private _slideOnLoad(): void {
-    this._timeoutId = window.setTimeout(() => {
-      this._el.nativeElement.classList.add(SLIDER_ACTIVE_CLASS);
-      window.setTimeout(() => {
-        this._el.nativeElement.classList.remove(this._sliderClass);
-      }, 3000);
-      this._timeoutId = 0;
-    }, this.slideDelay);
-  }
-
-  ngOnDestroy() {
-    if (this._timeoutId) {
-      window.clearTimeout(this._timeoutId);
-    }
+    this.renderer.setStyle(
+      this._el.nativeElement,
+      'animation',
+      `g-slide-in ${this.slideDuration}ms ease-in-out ${this.slideDelay}ms both`
+    );
   }
 }
