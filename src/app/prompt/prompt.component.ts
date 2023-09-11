9dff255298ac0dbdd5ba553ff7fc95e8c578333b
name <name@outlook.com>
2023-09-11 09:54:01 +0800

    prompt done.

--- src/app/prompt/prompt.component.ts
+++ src/app/prompt/prompt.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { NgxRerenderModule } from 'ngx-rerender';
 
 import { SliderDirective } from 'src/app/core/slider.directive';
 import { WheelComponent } from '../wheel/wheel.component';
@@ -14,15 +16,34 @@ export enum StatusEnum {
 @Component({
   selector: 'app-prompt',
   standalone: true,
-  imports: [CommonModule, SliderDirective, WheelComponent],
+  imports: [CommonModule, NgxRerenderModule, SliderDirective, WheelComponent],
   templateUrl: './prompt.component.html',
   styleUrls: ['./prompt.component.css'],
 })
 export class PromptComponent {
   @Input() prompt = '';
   @Input() status = StatusEnum.Idle;
+  @Output() submitted = new EventEmitter<void>();
+
+  constructor(private _deviceService: DeviceDetectorService) {}
+
+  get isLargeDevice(): boolean {
+    return this._deviceService.isDesktop();
+  }
 
   get isDone(): boolean {
     return this.status !== StatusEnum.Progress;
+  }
+
+  get isSuccessful(): boolean {
+    return this.status === StatusEnum.Successful;
+  }
+
+  get isFailed(): boolean {
+    return this.status === StatusEnum.Failed;
+  }
+
+  submit() {
+    this.submitted.emit();
   }
 }
