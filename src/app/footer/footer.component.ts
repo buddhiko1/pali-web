8428f27d347ce689aa63676288d3d10721c400ea
name <name@outlook.com>
2023-10-16 16:04:46 +0800

    chore: remove log

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
-import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
@@ -11,14 +11,17 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
   styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-  constructor(private _el: ElementRef, private _router: Router) {}
+  constructor(
+    private _el: ElementRef,
+    private _router: Router,
+  ) {}
 
   ngOnInit(): void {
     // ajsust footer position when route change
     this._router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         const activeUrl = event.url.split('/')[1];
-        if (activeUrl === "account") {
+        if (activeUrl === 'account') {
           this._el.nativeElement.style.position = 'fixed';
           this._el.nativeElement.style.bottom = '0';
           this._el.nativeElement.style.width = '100%';
