200196e83f710eaca9b39b8e2b9008ac3c92f450
name <name@outlook.com>
2023-10-16 15:59:12 +0800

    fixed footer when route to account.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,47 +3,29 @@ import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
-import { SliderDirective } from '../core/slider.directive';
-
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, AngularSvgIconModule, SliderDirective],
+  imports: [CommonModule, AngularSvgIconModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-  isVisible = false;
-
-  constructor(
-    private _el: ElementRef,
-    private _router: Router,
-  ) {}
+  constructor(private _el: ElementRef, private _router: Router) {}
 
   ngOnInit(): void {
     // ajsust footer position when route change
     this._router.events.subscribe((event) => {
-      if (event instanceof NavigationStart) {
-        this.isVisible = false;
-      }
       if (event instanceof NavigationEnd) {
-        setTimeout(() => {
-          this.adjustPosition();
-          this.isVisible = true;
-        }, 1000);
+        const activeUrl = event.url.split('/')[1];
+        if (activeUrl === "account") {
+          this._el.nativeElement.style.position = 'fixed';
+          this._el.nativeElement.style.bottom = '0';
+          this._el.nativeElement.style.width = '100%';
+        } else {
+          this._el.nativeElement.style.position = 'static';
+        }
       }
     });
-  }
-
-  adjustPosition() {
-    const docHeight = document.documentElement.scrollHeight;
-    const windowHeight = window.innerHeight;
-    if (docHeight <= windowHeight) {
-      this._el.nativeElement.style.position = 'fixed';
-      this._el.nativeElement.style.bottom = '0';
-      this._el.nativeElement.style.width = '100%';
-    } else {
-      this._el.nativeElement.style.position = 'static';
-    }
   }
 }
