1f081d0abe3c3ddfd610f9997d759dfcbe2e9886
name <name@outlook.com>
2023-10-29 17:26:21 +0800

    fix footer slug.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,6 @@
+import { delay } from 'rxjs';
 import { Component, OnInit, ElementRef } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
@@ -11,15 +12,17 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
   styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-  constructor(
-    private _el: ElementRef,
-    private _router: Router,
-  ) {}
+  constructor(private _el: ElementRef, private _router: Router) {}
 
   ngOnInit(): void {
-    // ajsust footer position when route change
     this._router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this._el.nativeElement.style.display = 'none';
+      }
+    });
+    this._router.events.pipe(delay(1000)).subscribe((event) => {
       if (event instanceof NavigationEnd) {
+        this._el.nativeElement.style.display = 'block';
         const activeUrl = event.url.split('/')[1];
         if (activeUrl === 'account') {
           this._el.nativeElement.style.position = 'fixed';
