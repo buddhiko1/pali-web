ceff8bd8865814819b1d9b0f4cde14e9896d8565
buddhiko <buddhiko@outlook.com>
2023-11-10 14:19:02 +0800

    optimize mark loader.

--- src/app/mark-loader/mark-loader.component.ts
+++ src/app/mark-loader/mark-loader.component.ts
@@ -3,13 +3,6 @@ import { CommonModule } from '@angular/common';
 import { CheckSvgComponent } from '../svg/check/check.component';
 import { CloseSvgComponent } from '../svg/close/close.component';
 
-export enum StatusEnum {
-  Idle = 'idle',
-  Loading = 'loading',
-  Successful = 'successful',
-  Failed = 'failed',
-}
-
 @Component({
   selector: 'app-mark-loader',
   standalone: true,
@@ -18,19 +11,12 @@ export enum StatusEnum {
   styleUrl: './mark-loader.component.css',
 })
 export class MarkLoaderComponent {
-  @Input() status = StatusEnum.Idle;
+  @Input() errorInfo = '';
+  @Input() successInfo = '';
   @HostBinding('style.--size')
   @Input()
   size = '1.5rem';
   @HostBinding('style.--borderWidth')
   @Input()
   borderWidth = '0.07rem';
-
-  get isSuccessful(): boolean {
-    return this.status === StatusEnum.Successful;
-  }
-
-  get isFailed(): boolean {
-    return this.status === StatusEnum.Failed;
-  }
 }
