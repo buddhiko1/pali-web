2f4e4c809ca7dd0c6cc558b9a9c50d1189e6d009
buddhiko <buddhiko@outlook.com>
2024-02-10 13:28:48 +0800

    blog-edtor: optimize style.

--- src/app/blogs/blog-viewer/blog-viewer.component.ts
+++ src/app/blogs/blog-viewer/blog-viewer.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 import { BlogsService } from '../blogs.service';
-import { StorageService } from 'src/app/shared/services/storage.service';
 import { SafeHtmlPipe } from 'src/app/shared/pipes/safe-html.pipe';
 import { PenSvgComponent } from 'src/app/svg/pen/pen.component';
 import { IconButtonComponent } from 'src/app/ui/icon-button/icon-button.component';
@@ -19,9 +18,7 @@ export class BlogViewerComponent implements OnInit {
   blog!: BlogFragment;
 
   constructor(
-    private _router: Router,
     private _activatedRoute: ActivatedRoute,
-    private _storageService: StorageService,
     private _blogsService: BlogsService,
   ) {}
 
@@ -32,16 +29,5 @@ export class BlogViewerComponent implements OnInit {
       .then((blog) => {
         this.blog = blog;
       });
-  }
-
-  routeToEditor() {
-    this._router.navigate(['/blogs/editor', { id: this.blog.id }]);
-  }
-
-  get isMyBlog() {
-    if (this._storageService.isLoggedIn && this.blog) {
-      return this.blog.user_created?.id === this._storageService.account.id;
-    }
-    return false;
   }
 }
